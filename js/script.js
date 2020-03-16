954fa50a6767b73d21628a07f8a3c9aa80dd0a30
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-16 17:20:53 +0900

    js 수정파일
    index 파일의 script를 script.js 파일로 옮김

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 $(function () {
 
 
+
+    
+// section 1
+
     var swiper = new Swiper('.swiper-container', {
         pagination: {
             el: '.swiper-pagination',
         },
     });
+
+
+// section 3
+
+    var swiper = new Swiper('.sect3_wrap .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: '.sect3_wrap .swiper-pagination',
+          clickable: true,
+        },
+      });
+
+
+
+
+
+
+
+
 })
