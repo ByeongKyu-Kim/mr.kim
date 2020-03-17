59bef675c2144470126834b1f69c4a077d94f96e
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-17 16:55:26 +0900

    section1, section3 병합
    html section 병합 하기

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-  var swiper = new Swiper(".sect1 .swiper-container", {
+  var swiper1 = new Swiper(".sect1 .swiper-container", {
     pagination: {
       el: ".sect1 .swiper-pagination",
     },
@@ -9,4 +9,53 @@ $(function() {
     },
     loop: true,
   })
+
+
+
+
+
+
+  var swiper3 = new Swiper('.sect3_wrap .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.sect3_wrap .swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
+
+
+
+
+  $(".sect3_menu1").click(function(){
+    $(".sect3_menu").removeClass("sect3_on")
+    $(this).addClass("sect3_on")
+    
+    $(".sect3_bottom").fadeOut()
+    $(".sect3_kimbab").fadeIn()
+  })
+
+
+  $(".sect3_menu2").click(function(){
+    $(".sect3_menu").removeClass("sect3_on")
+    $(this).addClass("sect3_on")
+
+    $(".sect3_bottom").fadeOut()
+    $(".sect3_dupbab").css("top","0px").fadeIn()
+  })
+
+
+  $(".sect3_menu3").click(function(){
+    $(".sect3_menu").removeClass("sect3_on")
+    $(this).addClass("sect3_on")
+
+    $(".sect3_bottom").fadeOut()
+    $(".sect3_noodle").css("top","0px").fadeIn()
+  })
+  
+ 
+
+
 })
