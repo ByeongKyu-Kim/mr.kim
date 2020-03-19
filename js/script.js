facf7598af47a4b5b4d2ae96a3aacf9ca4a5460e
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-19 16:20:02 +0900

    section7 작업 - bk
    section7 에 먹방 유튜버 swiper 사용하여 작업파일 업로드

--- js/script.js
+++ js/script.js
@@ -179,6 +179,29 @@ $(function() {
   //sect5
   //sect6
   //sect7
+
+  var galleryThumbs = new Swiper('.sect7_thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.sect7_top', {
+    spaceBetween: 10,
+    loop:true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
   //sect8
   //sect9
 })
