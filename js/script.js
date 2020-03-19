4b871c9e7fdef5a19d9d5d13c5dfaba50a67c9f0
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-19 19:27:40 +0900

    section8 ( index 수정 ) -bk
    section8에 index 문제로 html css js 수정건

--- js/script.js
+++ js/script.js
@@ -221,18 +221,15 @@ $(function() {
 
   //sect8
 
-  $(".sect8_menu li a").click(function(e) {
-    e.preventDefault()
+  $(".sect8_menu li").click(function() {
     
-    $(".sect8_menu li a").removeClass("on")
+    $(".sect8_menu li").removeClass("on")
     $(this).addClass("on")
 
     var idx = $(this).index()
 
     $(".sect8_con li").fadeOut()
-    $(".sect8_con li")
-      .eq(idx)
-      .fadeIn()
+    $(".sect8_con li").eq(idx).fadeIn()
   })
 
   //sect9
