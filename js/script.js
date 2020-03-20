657e97aea5fd881c065fe9fe88e66d7c3846c6be
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-20 18:47:36 +0900

    section3 수정 작업
    section3 수정작업

--- js/script.js
+++ js/script.js
@@ -31,114 +31,50 @@ $(function() {
 
   //////////////////////////////////// sect3 /////////////////////////////////
 
-  var swiper3 = new Swiper(".sect3_wrap .swiper-container", {
+  var swiper3 = new Swiper(".sect3_box .swiper-container",{
     slidesPerView: 3,
     spaceBetween: 30,
-    pagination: {
-      el: ".sect3_wrap .swiper-pagination",
-      clickable: true
-    }
   })
 
-  $(".sect3_menu1").click(function() {
+  $(".sect3_menu").click(function() {
     $(".sect3_menu").removeClass("sect3_on")
     $(this).addClass("sect3_on")
 
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_kimbab").fadeIn()
+    var idx = $(this).index()
+
+    $(".sect3_box > div").fadeOut()
+    $(".sect3_box > div").eq(idx).fadeIn()
   })
 
-  $(".sect3_menu2").click(function() {
-    $(".sect3_menu").removeClass("sect3_on")
-    $(this).addClass("sect3_on")
+  // var s3q
+  // s3q = 0
 
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_dupbab")
-      .css("top", "0px")
-      .fadeIn()
-  })
+  // $(".sect3_next").click(function() {
+  //   s3q++
+  //   if (s3q < 9) {
+  //     alert(s3q)
+  //     $(".sect3_scroll").animate({ "margin-left": "-=410px" })
+  //   } else {
+  //     alert(s3q)
+  //     s3q = 8
+  //     $(".sect3_scroll").animate({ "margin-left": "-=0px" })
+  //   }
+  // })
 
-  $(".sect3_menu3").click(function() {
-    $(".sect3_menu").removeClass("sect3_on")
-    $(this).addClass("sect3_on")
+  // $(".sect3_prev").click(function() {
+  //   s3q--
 
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_noodle")
-      .css("top", "0px")
-      .fadeIn()
-  })
+  //   if (s3q > -1) {
+  //     alert(s3q)
+  //     $(".sect3_scroll").animate({ "margin-left": "+=410px" })
+  //   } else {
+  //     alert(s3q)
+  //     s3q = 0
+  //     $(".sect3_scroll").animate({ "margin-left": "+=0px" })
+  //   }
+  // })
 
   //////////////////////////////////// sect4 ///////////////////////////////
-
-  // 김밥 메뉴 페이드인, 페이드 아웃
-  $(".sect3_menu1").click(function() {
-    $(".sect3_menu").removeClass("sect3_on")
-    $(this).addClass("sect3_on")
-
-    $(".sect3_dupbab, .sect3_noodle").removeClass("sect3_ko")
-    $(".sect3_kimbab").addClass("sect3_ko")
-
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_kimbab").fadeIn()
-  })
-  // 덮밥 메뉴 페이드인, 페이드 아웃
-  $(".sect3_menu2").click(function() {
-    $(".sect3_menu").removeClass("sect3_on")
-    $(this).addClass("sect3_on")
-
-    $(".sect3_kimbab, .sect3_noodle").removeClass("sect3_ko")
-    $(".sect3_dupbab").addClass("sect3_ko")
-
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_dupbab").fadeIn()
-  })
-
-  // 면 메뉴 페이드인, 페이드 아웃
-  $(".sect3_menu3").click(function() {
-    $(".sect3_menu").removeClass("sect3_on")
-    $(this).addClass("sect3_on")
-
-    $(".sect3_kimbab, .sect3_dupbab").removeClass("sect3_ko")
-    $(".sect3_noodle").addClass("sect3_ko")
-
-    $(".sect3_bottom").fadeOut()
-    $(".sect3_noodle").fadeIn()
-  })
-
-  // 음식 이미지 클릭하면 1.15배로 확대
-
-  $(".sect3_swiper").mouseenter(function() {
-    $(".sect3_swiper").removeClass("sect3_form")
-    $(this).addClass("sect3_form")
-  })
-
-  var s3q
-  s3q = 0
-
-  $(".sect3_next").click(function() {
-    s3q++
-    if (s3q < 9) {
-      alert(s3q)
-      $(".sect3_scroll").animate({ "margin-left": "-=410px" })
-    } else {
-      alert(s3q)
-      s3q = 8
-      $(".sect3_scroll").animate({ "margin-left": "-=0px" })
-    }
-  })
-
-  $(".sect3_prev").click(function() {
-    s3q--
-
-    if (s3q > -1) {
-      alert(s3q)
-      $(".sect3_scroll").animate({ "margin-left": "+=410px" })
-    } else {
-      alert(s3q)
-      s3q = 0
-      $(".sect3_scroll").animate({ "margin-left": "+=0px" })
-    }
-  })
 
   // 섹트4의 오토바이 그림위 배달안내 스크립트 / 아직 css하지 않은 상황. 수정 중에 있음 /
 
