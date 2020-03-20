e3864dff8ab9c302a878568d324953ba9346503a
joooo24 <soany13@nate.com>
2020-03-20 19:26:02 +0900

    sect8
    sect8

--- js/script.js
+++ js/script.js
@@ -221,10 +221,10 @@ $(function() {
 
   //sect8
 
-  $(".sect8_menu li a").click(function(e) {
+  $(".sect8_menu_l li").click(function(e) {
     e.preventDefault()
 
-    $(".sect8_menu li a").removeClass("on")
+    $(".sect8_menu li").removeClass("on")
     $(this).addClass("on")
 
     var idx = $(this).index()
@@ -232,6 +232,20 @@ $(function() {
     $(".sect8_con li").fadeOut()
     $(".sect8_con li")
       .eq(idx)
+      .fadeIn()
+  })
+
+  $(".sect8_menu_r li").click(function(e) {
+    e.preventDefault()
+
+    $(".sect8_menu li").removeClass("on")
+    $(this).addClass("on")
+
+    var idx = $(this).index()
+
+    $(".sect8_con li").fadeOut()
+    $(".sect8_con li")
+      .eq(idx + 3)
       .fadeIn()
   })
 
