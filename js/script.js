1c4aed6b0dc841f07a922952a75b24fd5ad5707b
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-03-26 18:51:38 +0900

    section4 수정건
    section4 수정건

--- js/script.js
+++ js/script.js
@@ -77,14 +77,10 @@ $(function() {
     slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
   })
 
   //sect5
