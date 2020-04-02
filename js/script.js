033d769983be7054bcc78089bf4b925d4ad39507
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-04-02 18:27:50 +0900

    Revert "."
    This reverts commit 6ca376edb6e69672ec4452d09b73901134a57956.

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ $(function() {
       
       $("header li a").removeClass("om")
       if (i <= 2) {
-        // console.log(i)
+        console.log(i)
         $("hd_nav1 li")
           .eq((i = i))
           .find("a")
@@ -130,22 +130,36 @@ $(function() {
   })
 
   //sect4
-
-
-  var swiper4 = new Swiper(".sect4 .swiper-container", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  })
-  
+  var w = $(window).width()
+  if (w <= 560) {
+    var swiper4 = new Swiper(".sect4 .swiper-container", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    })
+  } else if(w > 560){
+    var swiper4 = new Swiper(".sect4 .swiper-container", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    })
+  }
 
   //sect5
 
